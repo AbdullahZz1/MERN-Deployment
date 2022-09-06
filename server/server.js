4a121d4d8941d6167dfdd08a288a15a105d3e200
Abdullah Alzahrani <105579812+AbdullahZz1@users.noreply.github.com>
2022-09-06 14:30:50 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 require('./config/mongoose.config'); // This is new
-app.use(cors());
 app.use(express.json()); // This is new
 app.use(express.urlencoded({ extended: true })); // This is new
 require('./routes/exam.routes')(app);
